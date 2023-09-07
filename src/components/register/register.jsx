db9135cff327b767b76cdd0a6087955d8759307c
ALDBANDA <alfred.banda1986@gmail.com>
2023-09-07 09:30:42 +0100

    'completed parse error'

--- src/components/register/register.jsx
+++ src/components/register/register.jsx
@@ -1,101 +1,96 @@
-import { React, useState} from 'react';
+import React, {useState} from 'react';
 import '../styles/form.scss';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { parseErrors } from '../../utils/parseErrors';
 
 export default function register() {
-const [firstName, setFirstName] = useState('');
-const [lastName, setLastName] = useState('');
-const [email, setEmail] = useState('');
-const [password, setPassword] = useState('');
-const [confirmPassword, setConfirmPassword] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] =useState('');
 
-const [error, setError] = useState({});
+  const handleSubmit = async (e) => {
+    e.preventDefault();//prevent default submission process when submitting the page
+    
+    const data = {
+      firstName,
+      lastName,
+      email,
+      password,
+      username: email,
+    };
 
-//send data to backend
-const registerUser = async (person) => {
-  //validation confirm password
-  if (person.password !== person.confirmPassword) {
-    setError({ message: 'Passwords do not match'});
-    return;
-}
-
-const { response, error } = await createUser(person);
-if (response) {
-  //clear form fields
-  setFirstName('');
-  setLastName('');
-  setEmail('');
-   setPassword('');
-  setConfirmPassword('');
-  setError({});
-}
-if (error) {
-  setError(error);
-}
-};
-const handleSubmit = (e) => {
-  e.preventDefault();
-  const user = {
-    firstName,
-    lastName,
-    username: email,
-    email,
-    password,
-    confirmPassword,
+    try {
+        const res = await axios.post('http://localhost:1337/api/auth/local/register', data);
+        // reset our state
+        setFirstName('');
+        setLastName('');
+        setEmail('');
+        setPassword('');
+        setConfirmPassword('');
+    }catch (err) {
+      console.log(parseErrors(err));
+      // console.log(err);
+    }
   };
 
-  registerUser(user);
-};
-
-console.log(user);
   return (
-    <div class="form form--page">
-      <div class="form__group form__group--page">
-        <label class="form__label">First name</label>
-        <input class="form__field" 
+    <form className="form form--page" onSubmit={handleSubmit}>
+      <div className="form__group form__group--page">
+        <label className="form__label">First name</label>
+        <input 
+        className="form__field" 
         type="text" 
-        placeholder="First name" />
+        placeholder="First name"
         value={firstName}
-        onChange={(e) => setFirstName(e.target.value)}
+        onChange={(e) => setFirstName(e.target.value)} />
       </div>
 
-      <div class="form__group form__group--page">
-        <label class="form__label">Last name</label>
-        <input class="form__field"
-         type="text" 
-         placeholder="Last name" />
-         value={lastName}
-         onChange={(e) => setLastName(e.target.value)}
+      <div className="form__group form__group--page">
+        <label className="form__label">Last name</label>
+        <input className="form__field" 
+        type="text" 
+        placeholder="Last name"
+        value={lastName} 
+        onChange={(e) => setLastName(e.target.value)}/>
       </div>
 
-      <div class="form__group form__group--page">
-        <label class="form__label">Email</label>
-        <input class="form__field" type="text" 
-        placeholder="Email" />
+      <div className="form__group form__group--page">
+        <label className="form__label">Email</label>
+        <input className="form__field" 
+        type="text" 
+        placeholder="Email"
         value={email}
-        onChange={(e) => setEmail(e.target.value)}
+        onChange={(e) => setEmail(e.target.value)} />
       </div>
 
-      <div class="form__group form__group--page">
-        <label class="form__label">Choose password</label>
-        <input class="form__field" type="text" 
-        placeholder="Choose password" />
+      <div className="form__group form__group--page">
+        <label className="form__label">Choose password</label>
+        <input className="form__field" 
+        type="password" 
+        placeholder="Choose password"
         value={password}
-        onChange={(e) => setPassword(e.target.value)}
+        onChange={(e) => setPassword(e.target.value)} />
       </div>
 
-      <div class="form__group form__group--page">
-        <label class="form__label">Confirm Password</label>
-        <input class="form__field" type="text" placeholder="Confirm Password" />
+      <div className="form__group form__group--page">
+        <label className="form__label">Confirm Password</label>
+        <input className="form__field" 
+        type="password" 
+        placeholder="Confirm Password"
+        value={confirmPassword}
+        onChange={(e) => setConfirmPassword(e.target.value)} />
       </div>
 
-      <div class="form__group form__group--page">
-        <input class="form__btn" type="submit" value="Register" />
+      <div className="form__group form__group--page">
+        <input className="form__btn" type="submit" value="Register" />
       </div>
 
       <footer>
         Already have an account? <Link to='/login'>Login</Link>
       </footer>
-    </div>
+    </form>
   );
 }
