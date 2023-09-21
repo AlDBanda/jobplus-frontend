5d7bfc9fd6a3d7bfc33d69e53e1248d21abfaaeb
ALDBANDA <alfred.banda1986@gmail.com>
2023-09-21 16:31:23 +0100

    'refactored forgot-password'

--- src/components/forgot_password/forgot_password.jsx
+++ src/components/forgot_password/forgot_password.jsx
@@ -1,44 +1,59 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/form.scss';
-import axios from 'axios';
+
 import Alert from '../alert/alert';
-import { parseErrors } from '../../utils/parseErrors';
+
+import { useApi } from '../../hooks/useApi';
 
 
 export default function forgot_password() {
   const [email, setEmail] = useState('');
   const [alert, setAlert] = useState({});
 
+  const { post } = useApi();
 
+  const handleSuccess = () => {
+    //reset our state
+    setEmail('');
+    // set success alert
+    setAlert({
+      type: 'success',
+      message: 'Please check your email for further instructions.',
+    });
+  };
+
+ 
   const handleSubmit = async (e) => {
     e.preventDefault(); // Prevent default submission process when submitting the page
 
-    const data = {
-      email,
-    };
 
     // Checking if the function is being called correctly
     console.log('handleLogin called');
 
-    try {
-      const response = await axios.post(
-        'http://localhost:1337/api/auth/forgot-password', 
-        data
-      );
+    await post('auth/forgot-password', {
+      data: { email },
+      onSuccess: (res) => handleSuccess(),
+      onFailure: (err) => setAlert(err)
+    });
+    // try {
+    //   const response = await axios.post(
+    //     'http://localhost:1337/api/auth/forgot-password', 
+    //     data
+    //   );
       
-      //reset our state
-      setEmail('');
+    //   //reset our state
+    //   setEmail('');
     
 
-    //set success alert
-    setAlert({
-      type:'success',
-      message: 'Please check your email for further instructions',
-    })
-    } catch (err) {
-      setAlert(parseErrors(err));
-    }
+    // //set success alert
+    // setAlert({
+    //   type:'success',
+    //   message: 'Please check your email for further instructions',
+    // })
+    // } catch (err) {
+    //   setAlert(parseErrors(err));
+    // }
   };
 
 
