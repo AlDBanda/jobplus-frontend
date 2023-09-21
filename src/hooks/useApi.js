7fe52df602728fb92841b23c1efd237831afa6c5
ALDBANDA <alfred.banda1986@gmail.com>
2023-09-21 16:40:20 +0100

    'refactor'

--- src/hooks/useApi.js
+++ src/hooks/useApi.js
@@ -17,5 +17,8 @@ export const useApi = () => {
   };
   return {
     post: (endpoint, options) => request(endpoint, {...options, method: 'POST'}),
+    get: (endpoint, options) => request(endpoint, {...options, method: 'GET'}),
+    put: (endpoint, options) => request(endpoint, {...options, method: 'PUT'}),
+    delete: (endpoint, options) => request(endpoint, {...options, method: 'DELETE'}),
   }
 };
