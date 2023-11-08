8bbae1fccec3150445b27b0a0bb80501fb3778dc
ALDBANDA <alfred.banda1986@gmail.com>
2023-11-08 12:47:25 +0000

    'paginate'

--- src/components/listings/listings.jsx
+++ src/components/listings/listings.jsx
@@ -1,17 +1,64 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './listings.scss';
-
 import Paginate from '../paginate/paginate';
-
 import { StarSaved, Money, Location, Timer } from '../images';
+import { useApi } from '../../hooks/useApi';
 
+
+
+const MAX_PER_PAGE = 3
+
+// Default function component for displaying job listings
 export default function listings() {
+  // State variables to hold job listings and their metadata
+  const [jobs, setJobs] = useState([]);
+  const [meta, setMeta] = useState({});
+
+  // Access the get method from the useApi custom hook
+  const { get } = useApi();
+
+  // Function to handle the success response of the API call
+  const handleSuccess = (res) => {
+    const { entries, meta } = res.data;
+
+    // Set the received job listings and metadata to the state variables
+    setJobs(entries);
+    setMeta(meta);
+  };
+
+  // Function to fetch jobs from the API
+  const fetchJobs = async (page = 1) => {
+    // Request job data from the API
+    await get('jobs', {
+      onSuccess: (res) => handleSuccess(res),
+      params: {
+        // Specify parameters for the request (e.g., populate company, start, and limit)
+        'populate[company]': true,
+        'start': (page -1 ) * MAX_PER_PAGE, 
+        'limit': MAX_PER_PAGE, 
+      },
+    });
+  };
+
+  
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+  const handlePageChange = (pageNumber) => {
+    fetchJobs(pageNumber);
+  };
+
+  
+
   return (
     <section>
-      <a href="apply.html">
-        <div className="listing__card">
+      {jobs.map((job) => (
+        <div key={job.id} className="listing__card">
           <header className="listing__header">
-            <h1 className="listing__title">Regulatory Affairs Senior Manager</h1>
+            <h1 className="listing__title">
+              {job.title}
+              </h1>
             <img className="listing__saved" src={StarSaved} alt="" />
             <p className="listing__company">
               Posted by <span>Koco Media</span>
@@ -34,16 +81,15 @@ export default function listings() {
           </ul>
 
           <p className="listing__detail">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo,
-            asperiores ullam minus at <b>Read more...</b>
+           {job.description} <b>Read more...</b>
           </p>
 
           <a href="" className="listing__cta">
             Withdraw application
           </a>
         </div>
-      </a>
-      <Paginate />
+      ))}
+      <Paginate meta={meta.paginate} onPageChange={handlePageChange}/>
     </section>
   );
 }
--- src/components/paginate/paginate.jsx
+++ src/components/paginate/paginate.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import './paginate.scss';
 
-export default function Paginate() {
+export default function Paginate({ meta, onPageChange }) {
   return (
     <ul className="paginate">
-      <li>&lt; PREVIOUS</li>
-      <li>NEXT &gt;</li>
+      <li
+      className={meta?.hasPrevPage ? '' : 'disabled'}
+      onClick={() => meta?.hasPrevPage && onPageChange(meta?.currentPage -1)}
+      >&lt; PREVIOUS</li>
+
+      <div className='page-numbers'>
+        {Array.from({ length: meta?.totalPages })}
+      </div>
+
+      <li
+      className={meta?.hasNextPage ? '' : 'disabled'}
+      onClick={() => meta?.hasNextPage && onPageChange(meta?.currentPage + 1)}
+      >NEXT &gt;</li>
     </ul>
   );
 }
